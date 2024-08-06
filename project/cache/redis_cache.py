e9ded80952f31ac818e8feb0fec1f4de296cc385
jawad <ijawad725@gmail.com>
2024-08-06 17:01:32 +0300

    fix

--- project/cache/redis_cache.py
+++ project/cache/redis_cache.py
@@ -2,30 +2,32 @@ import redis
 from ..config.development import Development
 import json
 
-try: 
-    redis_client = redis.StrictRedis(
-        host=Development.REDIS_HOST,
-        password=Development.REDIS_PASSWORD,
-        port=Development.REDIS_PORT,
-        decode_responses=True,
-        db=0
-    )
-    if redis_client.ping():
-        print("Connection successfully")
-    else:
-        print("Connection unsuccessfully")
-except redis.ConnectionError as e:
-    print(f"Cannot connect to redis: {str(e)}")
-    redis_client = None
+class Rediscache:
+    def __init__(self):
+        try:
+            self.redis_client = redis.StrictRedis(
+                host=Development.REDIS_HOST,
+                password=Development.REDIS_PASSWORD,
+                port=Development.REDIS_PORT,
+                decode_responses=True,
+                db=0
+            )
+            if self.redis_client.ping():
+                print("Connection successful")
+            else:
+                print("Connection unsuccessful")
+        except redis.ConnectionError as e:
+            print(f"Cannot connect to Redis: {str(e)}")
+            self.redis_client = None
 
-def get_cache(cache_key):
-    if redis_client:
-        cached_data = redis_client.get(cache_key)
-        if cached_data:
-            return cached_data
-    return None
-    
-def set_cache(cache_key, data, expired_time=3600):
-    if redis_client:
-        json_data = json.dumps(data, ensure_ascii=False).encode("utf-8")
-        return redis_client.setex(cache_key, expired_time, json_data)
+    def get_cache(self, cache_key):
+        if self.redis_client:
+            cached_data = self.redis_client.get(cache_key)
+            if cached_data:
+                return cached_data
+        return None
+
+    def set_cache(self, cache_key, data, expired_time=3600):
+        if self.redis_client:
+            json_data = json.dumps(data, ensure_ascii=False).encode("utf-8")
+            return self.redis_client.setex(cache_key, expired_time, json_data)
