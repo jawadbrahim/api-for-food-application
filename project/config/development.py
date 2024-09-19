58b8e8f675af4579f5d8268aa6acec023ec0662d
jawad <ijawad725@gmail.com>
2024-09-19 15:02:00 +0300

    heroku

--- project/config/development.py
+++ project/config/development.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Development:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     # REDIS_HOST = os.getenv("REDIS_HOST")
     # REDIS_PORT =os.getenv("REDIS_PORT")
     # REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
